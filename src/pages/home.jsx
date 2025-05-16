202545aabde4bfaa8ff154fe5ea1708be99466f6
wassii-khan <wassiikhan933@gmail.com>
2025-05-16 23:42:43 +0500

    refactor: simplify Home component by removing Navbar and Chat; integrate user authentication display

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,23 +1,15 @@
 import React from "react";
-import Navbar from "../components/common/navbar";
-import Chat from "../components/chat";
+import { useAuth } from "../hooks";
 
+// Chat
 const Home = () => {
+  // auth
+  const { auth } = useAuth();
+
   return (
-    <>
-      <header>
-        <Navbar />
-      </header>
-      <main className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 mt-5">
-        <h1 className="text-center text-4xl font-bold text-indigo-500">
-          Realtime Chat App
-        </h1>
-        <p className="text-center text-gray-500 mt-2">
-          This is a simple chat application.
-        </p>
-        <Chat />
-      </main>
-    </>
+    <div className="p-4 rounded-lg">
+      <h3>Welcome! {auth?.user?.username || "No User Found"}</h3>
+    </div>
   );
 };
 
